2f931b62590a6b2d930041a5e1ffd5ee07f83b68
willychen9453 <95835793+willychen9453@users.noreply.github.com>
2022-01-13 10:47:00 +0800

    Create fetch_input.c

---
+++ fetch_input.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+printf("�п�J���ӼƦr�A�H�ťչj�}�C\n");
+
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int d = 0;
+    int e = 0;
+    scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+
+    //add scanf code to get data from input
+    //and feed those input into a, b, c, d, e, respectively.
+    //only ONE line code is you need
+    //please refer page 18 of slides
+
+    printf("a=%d\n", a);
+    printf("b=%d\n", b);
+    printf("c=%d\n", c);
+    printf("d=%d\n", d);
+    printf("e=%d\n", e);
+}
