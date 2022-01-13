95f51106c8cbda67daee577c6e3568a40739f7c2
willychen9453 <95835793+willychen9453@users.noreply.github.com>
2022-01-13 10:50:36 +0800

    Create 中繼器.c

---
+++ 中繼器.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int input;
+
+    printf("�C��J�@�ӼƦr�A�{���|�A�L�X�ۦP���Ʀr�C\n");
+    //fill some condition into the following while loop
+    //only ONE line code is you need
+    //please refer page 4 of homework request
+    while (scanf("%d", &input)) {
+        if (input == input) { //if you have got the answer
+            printf("%d\n", input);
+
+    }
+}
+}
