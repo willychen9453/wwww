a291a8295178233b3da733b1eac9be85b5afa0d2
willychen9453 <95835793+willychen9453@users.noreply.github.com>
2022-01-13 10:49:39 +0800

    Create unit_place.c

---
+++ unit_place.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("�п�J�@�Ӿ�ơJ");
+
+    int i;
+
+    scanf("%d", &i);
+
+    //line 12 is incomplete, refer to page 4 of Operation slides to achieve the requirement.
+    printf("�o�Ӿ�ƪ��Ӧ�ƬO%d\n", i%10);
+}
